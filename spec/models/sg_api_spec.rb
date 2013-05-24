e83a099261bea4e01501b6c5503a9cb8b1d8effe
Raoul de Villiers <raouldevilliers@gmail.com>
2013-05-24 18:49:09 +0200

    Added a few tests, still need to run them.

--- spec/models/sg_api_spec.rb
+++ spec/models/sg_api_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 describe SgApi do
-	describe "SG api transactions" do
-		it "should return data from the server with a correct id" do
-		  json_response = SgApi.get_sg_survey_responses(survey_id)
-		  json_response["result_ok"].should eq true 
+	describe 'SG api transactions' do
+		
+		it 'should not return responses from the server with an incorrect id' do
+		  json_response = SgApi.get_sg_survey_responses('aa')
+		  json_response['result_ok'].should eq true 
+		end
+
+		it 'should return responses from the server with a correct id' do
+		  json_response = SgApi.get_sg_survey_responses('1176375')
+		  json_response['result_ok'].should eq true 
+		end
+
+		it 'should return surveys from the server' do
+		  json_response = SgApi.get_sg_surveys
+		  json_response['result_ok'].should eq true 
 		end
 	  
 	end

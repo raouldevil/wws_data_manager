cdf75869274ae298fda2b7ba53192885140aa615
Raoul de Villiers <raouldevilliers@gmail.com>
2013-05-30 00:05:16 +0200

    Half of the parse_task tests pass...

--- spec/models/parse_task_spec.rb
+++ spec/models/parse_task_spec.rb
@@ -4,7 +4,7 @@ describe Question do
 	describe 'underscore' do
 	  it 'should return a word without special characters' do
 	    question = Question.new(id: 1, title: 'One small step?')
-	    underscore(question.title).should eq 'one_small_step'
+	    question.underscore(question.title).should eq 'one_small_step'
 	  end
 	end
 
@@ -19,9 +19,9 @@ describe SingleAnswer do
 	end
 
 	describe 'get_answer' do
-		it 'should return the answer array' do
-		  question = SingleAnswer.new(id: 1, answer_set: [['[question(1)]', 'Yes!']])
-	    self.get_answer.should eq 'Yes!'
+		it 'should return the answer' do
+		  single_answer = SingleAnswer.new(id: 1, answer_set: [['[question(1)]', 'Yes!']])
+	    single_answer.get_answer.should eq 'Yes!'
 		end
 	end
 end
@@ -30,14 +30,14 @@ describe FlatAnswer do
 	describe 'get_header' do
 		it 'should return a word without special characters' do
 		  flat_answer = FlatAnswer.new(id: 1, title: 'One small step?')
-		  single_answer.get_header.should eq 'one_small_step'
+		  flat_answer.get_header.should eq 'one_small_step'
 		end
 	end
 
 	describe 'get_answer' do
 		it 'should return the answer array' do
-		  flat_answer = FlatAnswer.new(id: 1, answer: [['[question(1), option(10022)', 'Yes'],['[question(1), option(10023)]', 'No']])
-	    flat_answer.get_answer.should eq 'Yes No'
+		  flat_answer = FlatAnswer.new(id: 1, answer_set: [['[question(1), option(10022)', 'Yes'],['[question(1), option(10023)]', 'No']])
+	    flat_answer.get_answer.should eq 'Yes No '
 		end
 	end
 end
@@ -53,24 +53,24 @@ describe MultiAnswer do
 
 	describe 'get_answer' do
 		it 'should return the answer array' do
-		  question = MultiAnswer.new(id: 1, answer: [['[question(1), option(10022)', 'Yes'],['[question(1), option(10023)]', 'No']])
-	    self.get_answer.should eq ['Yes', 'No']
+		  multi_answer = MultiAnswer.new(id: 1, answer_set: [['[question(1), option(10022)', 'Yes'],['[question(1), option(10023)]', 'No']])
+	    multi_answer.get_answer.should eq ['Yes', 'No']
 		end
 	end
 end
 
 describe ParseTask do
-	describe 'parse_survey_questions' do
+	describe 'parse_survey_questions_responses' do
 
 		before(:each) do
-		  @q_json_string = '{"result_ok":true, "data":/
-			  [/
-			  	{"id":1,"_subtype":"checkbox","title":{"English":"Question - Checkbox"},"options":[/
-			  		{"id":10010,"value":"One"},{"id":10011,"value":"Two"},/
-			  		{"id":10012,"title":{"English":"Three"},"value":"Three"}/
-			  	]},/
-			  	{"id":2,"_subtype":"textbox","title":{"English":"Question - String"}/
-			  ]/
+		  @q_json_string = '{"result_ok":true, "data":
+			  [
+			  	{"id":1,"_subtype":"checkbox","title":{"English":"Question - Checkbox"},"options":[
+			  		{"id":10010,"value":"One"},{"id":10011,"value":"Two"},
+			  		{"id":10012,"title":{"English":"Three"},"value":"Three"}
+			  	]},
+			  	{"id":2,"_subtype":"textbox","title":{"English":"Question - String"}
+			  ]
 			}'
 		end
 
@@ -83,24 +83,22 @@ describe ParseTask do
 		  q_array[2].id.should eq 2
 		  q_array[2].title.should eq 'Question - String'
 		end
-	  
 
-	end
-
-	describe 'parse_survey_responses' do
-	  r_json_string = '{"result_ok":true,"data":[/
-	  		{"id":"1","[question(1), option(10013)]":"One","[question(1), option(10013)]":"Two", "[question(2)]":"Test one"},/
-	  		{"id":"2","[question(1), option(10013)]":"One","[question(2)]":"Test two"},/
-	  	]/
-	  }'
-	  q_array = ParseTask.parse_survey_questions(@q_json_string)
-	  r_array = ParseTask.parse_survey_responses(q_array, r_json_string)
-	  r_array[1][1].class.should eq 'FlatAnswer'
-	  r_array[1][1].id.should eq 1
-	  r_array[1][1].answer_set.should eq [["[question(1), option(10013)]", "One"],["[question(1), option(10013)]", "Two"]]
-	  r_array[1][2].class.should eq 'SingleAnswer'
-		r_array[1][2].id.should eq 2
-		r_array[1][2].answer_set.should eq [["[question(2)]", "Test one"]]
+		it 'should parse the question responses' do
+		  r_json_string = '{"result_ok":true,"data":[
+		  		{"id":"1","[question(1), option(10013)]":"One","[question(1), option(10013)]":"Two", "[question(2)]":"Test one"},
+		  		{"id":"2","[question(1), option(10013)]":"One","[question(2)]":"Test two"},
+		  	]
+		  }'
+		  q_array = ParseTask.parse_survey_questions(@q_json_string)
+		  r_array = ParseTask.parse_survey_responses(q_array, r_json_string)
+		  r_array[1][1].class.should eq 'FlatAnswer'
+		  r_array[1][1].id.should eq 1
+		  r_array[1][1].answer_set.should eq [["[question(1), option(10013)]", "One"],["[question(1), option(10013)]", "Two"]]
+		  r_array[1][2].class.should eq 'SingleAnswer'
+			r_array[1][2].id.should eq 2
+			r_array[1][2].answer_set.should eq [["[question(2)]", "Test one"]]
+		end
 	end
 
 end
